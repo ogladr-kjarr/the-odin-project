c5a2db8c941fbf777248b9233cb0247ad4dd527c
ogladr-kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2023-10-12 15:33:49 +0100

    Add rock paper scissors ui

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -1,9 +1,3 @@
-function standardizeInput(mixedCase) {
-    let lowerCase = mixedCase.toLowerCase();  
-    let startIsUpperCase = lowerCase.charAt(0).toUpperCase() + lowerCase.substring(1);
-    return startIsUpperCase;
-  }
-
 function getComputerChoice(){
     let guessIndex = Math.floor(Math.random() * 3);
     switch(guessIndex) {
@@ -16,32 +10,121 @@ function getComputerChoice(){
     } 
 }
 
-function playRound(playerSelection, computerSelection){
-    let standerdizedPlayerSelection = standardizeInput(playerSelection);
+function setTextWindow(newText){
+    document.querySelector('.game-text').textContent = newText;
 
-    if(standerdizedPlayerSelection === "Rock"){
-        if(computerSelection === "Rock") return "Draw! Rock matches Rock";
-        else if(computerSelection === "Paper") return "You Lose: Paper beats Rock";
-        else return "You Win: Rock beats Scissors";
-    } else if(standerdizedPlayerSelection === "Paper"){
-        if(computerSelection === "Rock") return "You Win: Paper beats Rock";
-        else if(computerSelection === "Paper") return "Draw! Paper matches Paper";
-        else return "You Lose: Scissors beats Paper";
-    } else if(standerdizedPlayerSelection === "Scissors") {
-        if (computerSelection === "Rock") return "You Lose: Rock beats Paper";
-        else if(computerSelection === "Paper") return "You Win: Scissors beats Paper";
-        else return "Draw: Scissors matches Scissors";
+}
+
+function getWinnerTallyElement(winner){
+    if(winner === 'Human'){
+        return document.querySelector('.human-tally');
     } else {
-        return "Game nullified, Please enter only 'rock', or 'paper', or 'scissors'";
+        return document.querySelector('.computer-tally');
     }
 }
 
-function game() {
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Please enter either: rock, paper, scissors");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+function updateTallyTable(winner){
+    
+    if (winner !== "Draw"){
+        let elementToUpdate = getWinnerTallyElement(winner);
+        let currentScore = +elementToUpdate.textContent;
+        let newScore = currentScore + 1;
+        elementToUpdate.textContent = newScore;
     }
 }
 
-game();
+function checkGamesFinished(winner){
+
+    let winnerElement = getWinnerTallyElement(winner);
+    if (+winnerElement.textContent === 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function playRound(playerSelection, computerSelection){
+
+    let gameOutcome = {};
+
+    if(playerSelection === "Rock"){
+        if(computerSelection === "Rock") {
+            gameOutcome.text = "Draw! Rock matches Rock";
+            gameOutcome.winner = "Draw"
+        } 
+        else if(computerSelection === "Paper") {
+            gameOutcome.text = "You Lose: Paper beats Rock";
+            gameOutcome.winner = "Computer";
+        }
+        else {
+            gameOutcome.text = "You Win: Rock beats Scissors";
+            gameOutcome.winner = "Human";   
+        }
+    } else if(playerSelection === "Paper"){
+        if(computerSelection === "Rock") {
+            gameOutcome.text = "You Win: Paper beats Rock";
+            gameOutcome.winner = "Human";
+        } 
+        else if(computerSelection === "Paper") {
+            gameOutcome.text = "Draw! Paper matches Paper";
+            gameOutcome.winner = "Draw";
+        }
+        else {
+            gameOutcome.text = "You Lose: Scissors beats Paper";
+            gameOutcome.winner = "Computer";
+        } 
+    } else if(playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            gameOutcome.text = "You Lose: Rock beats Paper";
+            gameOutcome.winner = "Computer";
+        }
+        else if(computerSelection === "Paper") {
+            gameOutcome.text = "You Win: Scissors beats Paper";
+            gameOutcome.winner = "Human";
+        } 
+        else {
+            gameOutcome.text = "Draw: Scissors matches Scissors";
+            gameOutcome.winner = "Draw";
+        }
+    }
+    return gameOutcome;
+}
+
+function game(playerSelection) {
+    let gameOutcome = playRound(playerSelection, getComputerChoice());
+    setTextWindow(gameOutcome.text);
+    updateTallyTable(gameOutcome.winner);
+    if (checkGamesFinished(gameOutcome.winner)){
+        if (gameOutcome.winner === "Human"){
+            setTextWindow("Congratulations, you won the game set.")
+        } else {
+            setTextWindow("Commiserations, you lost the game set.")
+        }
+        document.querySelector('button#rock').removeEventListener('click', clickRock);
+        document.querySelector('button#paper').removeEventListener('click', clickPaper);
+        document.querySelector('button#scissors').removeEventListener('click', clickScissors);
+    }
+}
+
+function clickRock() {
+    game("Rock");
+}
+
+function clickPaper() {
+    game("Paper");
+}
+
+function clickScissors() {
+    game("Scissors");
+}
+
+let rockButton = document.querySelector('button#rock');
+const paperButton = document.querySelector('button#paper');
+const scissorsButton = document.querySelector('button#scissors');
+
+rockButton.addEventListener('click', clickRock);
+
+paperButton.addEventListener('click', clickPaper);
+
+scissorsButton.addEventListener('click', clickScissors);
